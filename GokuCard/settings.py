58225f40895ac7e97f64ff7344da57e142f0d211
arunNewPhoenix <62498648+arunNewPhoenix@users.noreply.github.com>
2021-11-11 22:15:18 +0530

    Update settings.py

--- GokuCard/settings.py
+++ GokuCard/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dpa_vv8ey&9igx#!f*#)f0b-sz19$@831e(5%#=*#6nzkil!qi'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
